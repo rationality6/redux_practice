ec4401cd95d7c0545bcc61e7a156fc3260fb56cb
Hyun Ahn <rationality6@gmail.com>
2016-09-18 09:53:49 -0700

    react-router. tuto

---
+++ react-codelab1/src/Topics.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link, Match } from 'react-router';
+
+const Empty = () => {
+    return(
+        <h3>Please select a topics</h3>
+    );
+};
+
+const Topic = ({params}) => {
+    return(
+        <div>
+            <h3>{params.topicId}</h3>
+        </div>
+    )
+}
+
+const Topics = ({pathname}) => {
+    return (
+        <div>
+            <h2>Topics</h2>
+            <ul>
+                <li><Link to={`${pathname}/rendering`}>Randering with react</Link></li>
+                <li><Link to={`${pathname}/components`}>Components</Link></li>
+                <li><Link to={`${pathname}/props-v-state`}>Props v. State</Link></li>
+            </ul>
+            <Match exactly pattern={pathname} component={Empty}/>
+            <Match pattern={`${pathname}/:topicId`} component={Topic}/>
+        </div>
+    )
+}
+
+export default Topics;
